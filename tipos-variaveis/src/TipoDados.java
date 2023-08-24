297b8e4efbd69f4519b9a3b4d7b1de4d106d2e9b
Rafael Rebello <rafael.a.rebellodev@gmail.com>
2023-08-23 22:00:26 -0300

    Tipos de dados

---
+++ tipos-variaveis/src/TipoDados.java
@@ -0,0 +1,9 @@
+public class TipoDados {
+
+    byte idade = 123;
+    short ano = 2023;
+    int cep = 20560060;
+    long dpf = 123456789112L;
+    float pi = 3.14F;
+    double salario = 2430.50;
+}
